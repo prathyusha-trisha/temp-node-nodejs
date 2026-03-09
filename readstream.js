5ba1aee0933ad8b6e42df3f0f21ee0b815a3856f
Prathyushap567 <prathyushap567@gmail.com>
2026-03-09 16:29:42 +0530

    Delete readstream.js

--- readstream.js
+++
@@ -1,9 +0,0 @@
-const {createReadStream} = require('fs');
-
-const stream = createReadStream('./content/big.txt',{encoding:'utf-8'});
-stream.on('data',(result)=>{
-    console.log(result);
-})
-stream.on('error',(err)=>{
-    console.log(err);
-})
